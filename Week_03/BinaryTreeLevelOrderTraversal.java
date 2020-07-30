2b670f6d1824263781f7d9716195b38ac9072495
kaithy.xu <617134312@qq.com>
2020-07-30 21:30:29 +0800

    Update BinaryTreeLevelOrderTraversal.java

--- Week_03/BinaryTreeLevelOrderTraversal.java
+++ Week_03/BinaryTreeLevelOrderTraversal.java
@@ -44,19 +44,5 @@ public class BinaryTreeLevelOrderTraversal {
 
         return result;
     }
-
-    public static void main(String[] args) {
-        TreeNode root = new TreeNode(3);
-        root.left = new TreeNode(9);
-        root.right = new TreeNode(20);
-
-        root.right.left = new TreeNode(15);
-        root.right.right = new TreeNode(7);
-
-        List<List<Integer>> result = new BinaryTreeLevelOrderTraversal().levelOrder(root);
-
-        for (List<Integer> list : result) {
-            System.out.println(list);
-        }
-    }
+    
 }
